70c67ef6a5754d69171144b019e39df0434779cf
Damien Allison <dka@google.com>
2019-11-10 14:13:25 +0000

    Added commit handler for underlying writer implementation abstraction e.g. CRUD or append log.

---
+++ src/main/java/digital/upgrade/replication/CommitHandler.java
@@ -0,0 +1,18 @@
+package digital.upgrade.replication;
+
+import static digital.upgrade.replication.Model.CommitMessage;
+
+/**
+ * CommitHandler is used to abstract the underlying writer implementation from the replicator
+ * by encapsulating the writer semantics.
+ */
+public interface CommitHandler {
+
+    /**
+     * Write a commit message to underlying persistent storage.
+     *
+     * @param commit message to write.
+     * @return true if the write was successful.
+     */
+    boolean write(CommitMessage commit);
+}
