4fb01799847337e43af02df0232b5d7b9c92cf39
Damien Allison <dka@google.com>
2019-12-06 20:58:08 +0000

    Added controller for raft protocol states.

---
+++ src/main/java/digital/upgrade/replication/raft/Controller.java
@@ -0,0 +1,8 @@
+package digital.upgrade.replication.raft;
+
+/**
+ * State controller which provides a delegate for the Raft replicator to implement
+ * different Raft server states e.g. follower, leader, candidate
+ */
+public interface Controller {
+}
